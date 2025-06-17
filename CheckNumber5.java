a0ac38c3d677f309a320fd6cdc248f65d9344b92
Akash K <akash178kusha@gmail.com>
2025-06-18 00:30:26 +0530

    Added MaxofTwo3.java, MinofTwo4.java, CheckNumber5.java

---
+++ CheckNumber5.java
@@ -0,0 +1,14 @@
+public class CheckNumber5 {
+    public static void main(String[] args) {
+        int number = -5;  // Change this value to test with other numbers
+
+        if (number > 0) {
+            System.out.println(number + " is Positive");
+        } else if (number < 0) {
+            System.out.println(number + " is Negative");
+        } else {
+            System.out.println("The number is Zero");
+        }
+    }
+    }
+
---
+++ MinofTwo4.java
@@ -0,0 +1,13 @@
+public class MinofTwo4 {
+     public static void main(String[] args) {
+        int a = 15;
+        int b = 30;
+
+        if (a < b) {
+            System.out.println(a + " is the smaller number");
+        } else {
+            System.out.println(b + " is the smaller number");
+        }
+    }
+}
+
